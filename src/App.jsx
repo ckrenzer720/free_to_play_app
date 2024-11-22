b0c0db542e8badff7ffb9abc2278ff3d8862ab33
Conner Krenzer <krenzer84@gmail.com>
2024-11-22 16:38:37 -0500

    setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <>
+      <div></div>
+      <h1>FTP Games</h1>
+      <div className="card"></div>
+    </>
+  );
+}
+
+export default App;
